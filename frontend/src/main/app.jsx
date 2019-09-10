4fb4fe050f008114c6e6961bec348f6fa022ef6f
gdss18007 <gabriel.santossilva.18007@gmail.com>
2019-09-10 01:19:10 -0300

    10/set

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,8 +1,11 @@
 import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 import React from 'react'
+import Menu from '../template/menu'
+import Routes from '../main/routes'
 export default props =>(
     <div className='container'>
-        <h1>Teste</h1>
+        <Menu/>
+        <Routes/>
     </div>
 )
---
+++ frontend/src/todo/todo.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+export default class Todo extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Clinica passo para o inferno</h1>
+            </div>
+            )
+    }
+}
